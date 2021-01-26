fcefe21b4105ff692a8f68fd701ab6f5e39eedda
LeonardoBenhumea <leonardo4509@live.com.mx>
2021-01-26 12:55:03 -0600

    26-01-21

--- ADTS/Arboles/arboles_binarios.py
+++ ADTS/Arboles/arboles_binarios.py
@@ -38,15 +38,89 @@ class BinarySearchTree:
     def __recorrido_in( self , nodo ):
         if nodo != None:
             self.__recorrido_in( nodo.left )
-            print(nodo.data)
+            print( nodo.data , end=" , ")
             self.__recorrido_in( nodo.right )
 
-    def transversal( self , format="inorden"):
+    def __recorrido_pos( self , nodo ):
+        if nodo:
+            self.__recorrido_pos(nodo.left)
+            self.__recorrido_pos(nodo.right)
+            print(nodo.data, end=", ")
+
+    def __recorrido_pre( self , nodo ):
+        if nodo:
+            print(nodo.data, end=", ")
+            self.__recorrido_pre( nodo.left )
+            self.__recorrido_pre( nodo.right )
+
+    def transversal(self,format="inorden"):
         if format == "inorden":
-            self.__recorrido_in( self.__root )
+            print("inorden")
+            self.__recorrido_in(self.__root)
         elif format == "preorden":
-            print("Recorrido en pre")
+            print("preorden")
+            self.__recorrido_pre(self.__root)
         elif format == "posorden":
-            print("Posorden")
+            print("posorden")
+            self.__recorrido_pos(self.__root)
         else:
-            print("Error, ese formato no existe")  
+            print("Error, formato incompatible")
+        print()
+
+    def search( self , value ):
+        if self.__root == None:
+            return None
+        else:
+            return self.__search( self.__root , value )
+
+    def __search( self , nodo , value ):
+        if nodo == None: # vacio ??? caso base de recursividad
+            print("Caso base")
+            return None
+        elif nodo.data == value: # caso base de recursividad
+            print("Encontrado")
+            return nodo
+        elif value < nodo.data:
+            print("Buscar a la izq.")
+            return self.__search( nodo.left , value )
+        else:
+            print("Buscar a la derecha")
+            return self.__search( nodo.right , value )
+
+    def remove( self , value ):
+        if self.__root == None:
+            print("Nada que eliminar")
+        else:
+            self.__remove( None , None , self.__root , value ) #
+
+    def __remove( self , padre_hi , padre_hd , actual , value ):
+        if actual == None:
+            print("Caso base")
+            return None
+        elif actual.data == value: #caso base
+            print("Encontrado:" , actual.data)
+            #Caso 1: hoja
+            if actual.left == None and actual.right == None:
+                if padre_hi != None: #
+                    padre_hi.left = None
+                else:
+                    padre_hd.right =  None
+            #caso 2: con un hijo
+            if (actua.left != None and actual.right == None) or (actua.left == None and actual.right != None):
+                print("Es un nodo con un hijo", actual.data)
+                if actual.left != None:
+                    actual.data = actual.left.data
+                    actual.left = None
+                else:
+                    actual.data = actual.right.data
+                    actual.right = None
+
+            #caso 3: con los dos hijos
+
+            #return actual
+        elif value < actual.data:
+            print("Buscar a la izquierda")
+            self.__remove( actual , None , actual.left , value )
+        else:
+            print("Buscar a la derecha")
+            self.__remove( None , actual , actual.right , value )
--- ADTS/Arboles/prueba_arbol.py
+++ ADTS/Arboles/prueba_arbol.py
@@ -1,11 +1,25 @@
 from arboles_binarios import BinarySearchTree
-
 abb = BinarySearchTree()
 abb.insert(50)
 abb.insert(30)
 abb.insert(60)
-abb.insert(35)
-abb.insert(89)
+abb.insert(15)
+abb.insert(23)
+abb.insert(110)
+abb.insert(90)
+abb.insert(120)
+abb.insert(100)
+abb.insert(55)
+abb.insert(58)
+abb.insert(115)
 
-
+abb.transversal() #inorden
+abb.remove( 90 )
 abb.transversal()
+"""
+abb.transversal()
+abb.transversal("preorden")
+abb.transversal("posorden")
+res = abb.search(35)
+print(f"El reultado es:{ res.data }")
+"""
